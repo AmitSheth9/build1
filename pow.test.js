33c9bb37515ca4a1d4dba76ea71ae132b5934b13
AmitSheth9 <amit.sheth9@gmail.com>
2021-10-29 13:20:16 -0700

    wrote passing function that uses exponentiation, not much to refactor

--- pow.test.js
+++ pow.test.js
@@ -3,8 +3,8 @@ const baseExp = require('./pow');
 
 describe('baseExp', () => {
     it('returns base to the exponent power', () => {
-            const base = 5;
+            const base = 6;
             const exponent = 3
-        expect(baseExp(base, exponent)).toEqual(5*5*5);
+        expect(baseExp(base, exponent)).toEqual(6*6*6);
     })
 })
