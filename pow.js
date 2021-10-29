a03818cfd30bd798c29fcb0f441006ba02fb2808
AmitSheth9 <amit.sheth9@gmail.com>
2021-10-29 13:15:28 -0700

    passing test using Math.pow, not much to refactor here

--- pow.js
+++ pow.js
@@ -1,7 +1,7 @@
 //create a function that takes a base and exponent and raises the base to the exponent power
 
 const baseExponent = (base, exponent) => {
-
+    return Math.pow(base, exponent);
 }
 
 module.exports = baseExponent;
